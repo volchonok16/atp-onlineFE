0bbb21d0da4baa6d812d04b9d8e2c67394b4e0ea
natikana <mashkovski82@gmail.com>
2023-10-07 19:09:58 +0200

    update: update order/booking(type, naming) and filtration

--- src/features/order/tabs/dataForShippingDocs/Table/Table.tsx
+++ src/features/order/tabs/dataForShippingDocs/Table/Table.tsx
@@ -25,23 +25,41 @@ export const Table: FC<PropsType> = ({ data }) => {
               <th>Время подачи ТС</th>
               <th>Время окончания</th>
               <th>Кол-во часов подачи</th>
+              <th>Общее количество часов</th>
+              <th>Тариф для заказчика</th>
+              <th>Сумма для заказчика</th>
+              <th>Тариф для подрядчика</th>
+              <th>Кол-во часов для подрядчика</th>
+              <th>Сумма</th>
+              <th>Прибыль с поездки подрядчика</th>
+              <th>Комментарий</th>
+              <th>Второй водитель</th>
             </tr>
           </thead>
           <tbody>
             {data.map((data, i) => {
               return (
                 <tr key={i}>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                  <td>{data.ZAK1}</td>
+                  <td>{data.ORG_NAME}</td>
+                  <td>{data.PRIVL_TRANSPORT}</td>
+                  <td>{data.RAZOV}</td>
+                  <td>{data.FLIGHT}</td>
+                  <td>{data.T_T}</td>
+                  <td>{data.MAM}</td>
+                  <td>{data.FULL_FIO}</td>
+                  <td>{data.VR_V}</td>
+                  <td>{data.VR_Z}</td>
+                  <td>{data.VR_I}</td>
+                  <td>{data.SUMM_VREM}</td>
+                  <td>{data.CENA}</td>
+                  <td>{data.SUMM}</td>
+                  <td>{data.CENA_PODR}</td>
+                  <td>{data.VREM_I_PODR}</td>
+                  <td>{data.SUMM_PODR}</td>
+                  <td>{data.PROFIT_PODR}</td>
+                  <td>{data.COMMENTAR}</td>
+                  <td>{data.FULL_FIO2}</td>
                 </tr>
               )
             })}
