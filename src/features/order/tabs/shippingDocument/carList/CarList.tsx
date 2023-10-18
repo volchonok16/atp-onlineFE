524b30de67ec0f480fb01887c17209559339ef35
AlucarD <skoriy12990@gmail.com>
2023-10-18 08:28:38 +0300

    add placeholder for empty car list

--- src/features/order/tabs/shippingDocument/carList/CarList.tsx
+++ src/features/order/tabs/shippingDocument/carList/CarList.tsx
@@ -38,36 +38,45 @@ export const CarList: FC<Props> = ({ carList }) => {
     getCoreRowModel: getCoreRowModel(),
   })
 
+  const emptyTable = <p>Данные отсутствуют</p>
+
   return (
     <div>
       <ScrollableTableWrapper height="70vh">
-        <table className={css.table}>
-          <thead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th key={header.id}>
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext(),
-                    )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {table.getRowModel().rows.map((row) => (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        {!carList ? (
+          emptyTable
+        ) : (
+          <table className={css.table}>
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th key={header.id}>
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows.map((row) => (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </ScrollableTableWrapper>
       <FilterTools
         label="Фильтр"
