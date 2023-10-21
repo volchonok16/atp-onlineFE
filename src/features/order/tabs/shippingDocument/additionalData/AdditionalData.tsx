1303d8a92b80472c15c617cb1c2f25e0880d8b34
AlucarD <skoriy12990@gmail.com>
2023-10-21 12:24:16 +0300

    add additional info block

--- src/features/order/tabs/shippingDocument/additionalData/AdditionalData.tsx
+++ src/features/order/tabs/shippingDocument/additionalData/AdditionalData.tsx
@@ -2,21 +2,27 @@ import * as Tabs from '@radix-ui/react-tabs'
 
 import css from './additionalInfo.module.scss'
 
-import { ScrollableTableWrapper } from 'src/common/table/scrollableTableWrapper/ScrollableTableWrapper'
+import { ProductSection } from 'src/features/order/tabs/shippingDocument/productSection/MyProductSection'
 
 export const AdditionalData = () => {
   return (
-    <Tabs.Root defaultValue="tab1" orientation="vertical">
+    <Tabs.Root
+      defaultValue="tab1"
+      orientation="vertical"
+      className={css.rootElement}
+    >
       <Tabs.List aria-label="additional information" className={css.tabList}>
         <Tabs.Trigger value="tab1">Товарный раздел</Tabs.Trigger>
         <Tabs.Trigger value="tab2">Транспортный раздел</Tabs.Trigger>
         <Tabs.Trigger value="tab3">Данные для ТН</Tabs.Trigger>
       </Tabs.List>
-      <ScrollableTableWrapper height="11.5rem">
-        <Tabs.Content value="tab1">Tab one content</Tabs.Content>
+      <div className={css.contentWrapper}>
+        <Tabs.Content value="tab1">
+          <ProductSection />
+        </Tabs.Content>
         <Tabs.Content value="tab2">Tab two content</Tabs.Content>
         <Tabs.Content value="tab3">Tab three content</Tabs.Content>
-      </ScrollableTableWrapper>
+      </div>
     </Tabs.Root>
   )
 }
