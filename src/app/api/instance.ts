03a1ae298620f9602da51a0ed7e6263311893567
NickPaferov <paferov.nikolai@gmail.com>
2023-10-28 16:05:26 +0300

    fix base URL

--- src/app/api/instance.ts
+++ src/app/api/instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'http://adjnatec.ru:4000/api',
+  baseURL: 'http://adjnatec.ru:4001/api',
   withCredentials: true,
 })
